e365394d65c1acce3dffede5a7766e9458715956
Ryan Endacott <rzeg24@gmail.com>
2013-07-22 21:15:35 -0500

    Added some basic tests

--- spec/verbal_expressions_spec.rb
+++ spec/verbal_expressions_spec.rb
@@ -1,6 +1,68 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "VerEx" do
-  it "" do
+describe VerEx do
+
+  describe '#find' do
+
+    let(:matcher) do
+      VerEx.new do
+        find 'lions'
+      end
+    end
+
+    it 'should correctly build find regex' do
+      matcher.source.should == '(lions)'
+    end
+
+    it 'should correctly match find' do
+      matcher.match('lions').should be_true
+    end
+
+    it 'should match part of a string with find' do
+      matcher.match('lions, tigers, and bears, oh my!').should be_true
+    end
+
+    it 'should only match the `find` part of a string' do
+      matcher.match('lions, tigers, and bears, oh my!')[0].should == 'lions'
+    end
+  end
+
+  describe 'URL Regex Test' do
+
+    let(:matcher) do
+      VerEx.new do
+        start_of_line
+        find 'http'
+        maybe 's'
+        find '://'
+        maybe 'www.'
+        anything_but ' '
+        end_of_line
+      end
+    end
+
+    it 'successfully builds regex for matching URLs' do
+      matcher.source.should == '^(http)(s)?(\:\/\/)(www\.)?([^\ ]*)$'
+    end
+
+    it 'matches regular http URL' do
+      matcher.match('http://google.com').should be_true
+    end
+
+    it 'matches https URL' do
+      matcher.match('https://google.com').should be_true
+    end
+
+    it 'matches a URL with www' do
+      matcher.match('https://www.google.com').should be_true
+    end
+
+    it 'fails to match when URL has a space' do
+      matcher.match('http://goo gle.com').should be_false
+    end
+
+    it 'fails to match with htp:// is malformed' do
+      matcher.match('htp://google.com').should be_false
+    end
   end
 end
